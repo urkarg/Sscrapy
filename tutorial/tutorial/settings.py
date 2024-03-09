a8b84377b22c4be41112bdc0a22200653e02b639
Andrei <amariucaiandrei@gmail.com>
2024-03-09 13:59:45 +0200

    Podu ros 3 9 work

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = "tutorial.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "tutorial (+http://www.yourdomain.com)"
+USER_AGENT = "Urkarg"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
--- tutorial/tutorial/spiders/imob_spider.py
+++ tutorial/tutorial/spiders/imob_spider.py
@@ -8,13 +8,15 @@ class ImobSpider(scrapy.Spider):
 
     def start_requests(self):
         urls = [
-            "https://storia.ro/ro/oferta/nicolina-biserica-catolica-4-camere-decomandat-3-balcoane-IDsOdz.html",
+            "https://storia.ro/ro/oferta/vanzare-apartament-2-camere-podu-ros-aproape-de-sensul-giratoriu-IDzvZo",
+            "https://storia.ro/ro/rezultate/vanzare/apartament/iasi?limit=36&ownerTypeSingleSelect=ALL&daysSinceCreated=1&by=DEFAULT&direction=DESC&viewType=listing",
+            "https://storia.ro/ro/companii/agentii/top-imobil-ID3949079",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-4]
+        page = response.url.split("/")[-2]
         filename = f"imob-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
