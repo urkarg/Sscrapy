dd1060f0ffa7485d0ac4c4648c8be130f188dce7
Andrei <amariucaiandrei@gmail.com>
2024-03-11 00:21:29 +0200

    imob json

--- tutorial/tutorial/spiders/imob_spider.py
+++ tutorial/tutorial/spiders/imob_spider.py
@@ -9,14 +9,18 @@ class ImobSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             "https://storia.ro/ro/oferta/vanzare-apartament-2-camere-podu-ros-aproape-de-sensul-giratoriu-IDzvZo",
-            "https://storia.ro/ro/rezultate/vanzare/apartament/iasi?limit=36&ownerTypeSingleSelect=ALL&daysSinceCreated=1&by=DEFAULT&direction=DESC&viewType=listing",
-            "https://storia.ro/ro/companii/agentii/top-imobil-ID3949079",
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
+    """ 
     def parse(self, response):
         page = response.url.split("/")[-2]
         filename = f"imob-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
+    """
+    def parse(self, response):
+        yield {
+            "Titlu": response.css("h1.efcnut38::text").get()
+        }
